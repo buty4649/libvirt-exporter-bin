0e5a05b72c1ff8806c3a99f76c2ba2300d62c9e4
Yuki Koya <ykky@pepabo.com>
2022-03-09 12:25:33 +0900

    fix container

--- build.sh
+++ build.sh
@@ -11,8 +11,16 @@ build() {
   local os_ver=$1
 
   pushd libvirt-exporter
-  ./build-with ./build_containers/Dockerfile.ubuntu${os_ver}
+
+  local container_name=libvirtexporterbuild:${os_ver}
+  docker build -t ${container_name} -f build_containers/Dockerfile.ubuntu${os_ver} .
+  docker run --rm \
+    -v "$PWD":/libvirt-exporter -w /libvirt-exporter \
+    -e GOOS=linux \
+    -e GOARCH=amd64 \
+    ${container_name} go build -mod vendor -o libvirt-exporter
   tar cvf - libvirt-exporter | gzip -9f - > ../libvirt-exporter-${VERSION}-${os_ver}.tar.gz
+
   popd
 }
 
